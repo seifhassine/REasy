d8e80b72370bba72eb245d0abcec70798deb8328
seifhassine <seifhassine@gmail.com>
2025-07-02 13:22:57 +0200

    v0.3

--- REasy.py
+++ REasy.py
@@ -764,7 +764,7 @@ class FileTab:
 class REasyEditorApp(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("REasy Editor v0.2.9")
+        self.setWindowTitle("REasy Editor v0.3.0")
         set_app_icon(self)
 
         try:
